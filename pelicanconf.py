7450464eae3b8f92d957b8fcf7d544bb96ee0047
Uldis Bojars <captsolo@gmail.com>
2021-12-05 22:02:36 +0200

    Improved the website.

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,10 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Uldis\' homepage', 'http://captsolo.net/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/CaptSolo'),)
 
 DEFAULT_PAGINATION = 10
 
